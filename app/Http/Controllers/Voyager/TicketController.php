aaf06bc092785d1434fd526a64930b187bfad964
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2021-11-20 14:45:37 +0700

    Test Role

--- app/Http/Controllers/Voyager/TicketController.php
+++ app/Http/Controllers/Voyager/TicketController.php
@@ -38,6 +38,9 @@ class TicketController extends \TCG\Voyager\Http\Controllers\VoyagerBaseControll
 
         $user = Auth::user();
         $role_group = DB::table("user_roles")->where('user_id', $user->id)->get();
+        if (count($role_group) == 0) {
+            $role_group[0]->role_id = $user->role_id;
+        }
         $slug = $this->getSlug($request);
 
         // GET THE DataType based on the slug
