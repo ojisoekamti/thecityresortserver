6eaae328531e35a3d9af572f5c63652e2edcba6f
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2021-11-23 11:40:25 +0700

    Update Logo

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -13,51 +13,6 @@
         @php
             $image_str = str_replace('\\', '/', setting('admin.logo_security'));
         @endphp
-
-        <?php
-        
-        function hari_ini()
-        {
-            $hari = date('D');
-        
-            switch ($hari) {
-                case 'Sun':
-                    $hari_ini = 'Minggu';
-                    break;
-        
-                case 'Mon':
-                    $hari_ini = 'Senin';
-                    break;
-        
-                case 'Tue':
-                    $hari_ini = 'Selasa';
-                    break;
-        
-                case 'Wed':
-                    $hari_ini = 'Rabu';
-                    break;
-        
-                case 'Thu':
-                    $hari_ini = 'Kamis';
-                    break;
-        
-                case 'Fri':
-                    $hari_ini = 'Jumat';
-                    break;
-        
-                case 'Sat':
-                    $hari_ini = 'Sabtu';
-                    break;
-        
-                default:
-                    $hari_ini = 'Tidak di ketahui';
-                    break;
-            }
-        
-            return $hari_ini;
-        }
-        ?>
-
         <tr>
             <td style="text-align: left;width:20%"><img src="{{ url('storage') }}/{{ $image_str }}"
                     style="width: 70px" alt=""></td>
