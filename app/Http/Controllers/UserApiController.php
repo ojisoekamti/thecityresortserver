6fb4e1bd040bba3bee7cca50f2c01b85e6fc09c8
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2021-11-28 11:23:32 +0700

    update Multiple

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -122,10 +122,10 @@ class UserApiController extends Controller
             $tukarShift = SwitchPermission::where("pemohon", $uid)
                 ->where('switch_permissions.`status`', '!=', 4)
                 ->where(function ($query) {
-                    $query->where('date', '>=', date("Y-m-d"));
+                    $query->where('switch_permissions.`date`', '>=', date("Y-m-d"));
                     $query->orWhere(function ($q) {
-                        $q->where('date_to', '>=', date("Y-m-d"))
-                            ->orWhereNull('date_to');
+                        $q->where('switch_permissions.`date_to`', '>=', date("Y-m-d"))
+                            ->orWhereNull('switch_permissions.`date_to`');
                     });
                 })
                 ->first();
@@ -135,10 +135,10 @@ class UserApiController extends Controller
                 $tukarShift = SwitchPermission::where("delegate", $uid)
                     ->where('switch_permissions.`status`', '!=', 4)
                     ->where(function ($query) {
-                        $query->where('date', '>=', date("Y-m-d"));
+                        $query->where('switch_permissions.`date`', '>=', date("Y-m-d"));
                         $query->orWhere(function ($q) {
-                            $q->where('date_to', '>=', date("Y-m-d"))
-                                ->orWhereNull('date_to');
+                            $q->where('switch_permissions.`date_to`', '>=', date("Y-m-d"))
+                                ->orWhereNull('switch_permissions.`date_to`');
                         });
                     })
                     ->first();
