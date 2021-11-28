b1ba3008367fc9b0c08ddd480824b7ffad5ed262
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2021-11-28 11:10:12 +0700

    update Multiple

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -133,12 +133,9 @@ class UserApiController extends Controller
                 // $unitData = Unit::select('unit_number')->where('id', $value->id_unit)->get();
             } else {
                 $tukarShift = SwitchPermission::where("delegate", $uid)
-                    ->where('switch_permission', '!=', 4)
+                    ->where('switch_permissions.`status`', '!=', 4)
                     ->where(function ($query) {
-                        $query->where(function ($q) {
-                            $q->where('date', '>=', date("Y-m-d"))
-                                ->orWhereNull('date');
-                        });
+                        $query->where('date', '>=', date("Y-m-d"));
                         $query->orWhere(function ($q) {
                             $q->where('date_to', '>=', date("Y-m-d"))
                                 ->orWhereNull('date_to');
