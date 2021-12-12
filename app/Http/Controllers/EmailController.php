dfcd30e73698dacc4ce8a456baf74b742a90c671
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2021-12-12 17:16:28 +0700

    insert Send Mail

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -10,8 +10,8 @@ class EmailController extends Controller
 {
     public function index($email)
     {
-        dump($email);
-        //Mail::to('anggota1.seccrr@gmail.com')->send(new SendEmail);   
+        //dump($email);
+        Mail::to($email)->send(new SendEmail);
         return [];
     }
 }
